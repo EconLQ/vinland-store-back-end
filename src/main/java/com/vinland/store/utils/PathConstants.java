d89723f25fd9c12f5ae2c0aa901376816b1c619a
EconLQ <zavliybogdan@gmail.com>
2025-01-23 22:28:12 +0200

    pathname constants

---
+++ src/main/java/com/vinland/store/utils/PathConstants.java
@@ -0,0 +1,13 @@
+package com.vinland.store.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PathConstants {
+    public static final String API_VERSION = "/api/v1";
+    public static final String AUTH = API_VERSION + "/auth";
+    public static final String BLOG = API_VERSION + "/blogs";
+    public static final String FRONT_END_DEV = "http://localhost:4200";
+    public static final String FRONT_END_PROD = "https://vinland-store.netlify.app/";
+
+}
