fd270c2590d81561eb0b8d7c80b9267a1a06da31
EconLQ <zavliybogdan@gmail.com>
2025-01-24 23:21:57 +0200

    web config

---
+++ src/main/java/com/vinland/store/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.vinland.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorize -> authorize.anyRequest().permitAll()
+                )
+                //.oauth2Login(auth -> auth.successHandler(oAuth2SuccessHandler))
+                .build();
+    }
+}
