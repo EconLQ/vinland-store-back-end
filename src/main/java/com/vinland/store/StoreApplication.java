cc33116b728efb1d741b2b68785fb5492795eac5
EconLQ <zavliybogdan@gmail.com>
2025-01-24 23:23:19 +0200

    registered beans

--- src/main/java/com/vinland/store/StoreApplication.java
+++ src/main/java/com/vinland/store/StoreApplication.java
@@ -1,7 +1,11 @@
 package com.vinland.store;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class StoreApplication {
@@ -10,4 +14,13 @@ public class StoreApplication {
         SpringApplication.run(StoreApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 }
