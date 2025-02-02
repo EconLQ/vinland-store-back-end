476cb434e6982e8f18dc3fcf7d921a7682560805
EconLQ <zavaliybogdan@gmail.com>
2025-02-02 21:35:56 +0200

    updated user

--- src/main/java/com/vinland/store/web/user/dao/UserDAO.java
+++ src/main/java/com/vinland/store/web/user/dao/UserDAO.java
@@ -11,4 +11,7 @@ import java.util.Optional;
 public interface UserDAO extends JpaRepository<User, Long> {
     @Query(value = "SELECT u FROM User u WHERE u.username = :username")
     Optional<User> findByUsername(String username);
+
+    @Query(value = "SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/vinland/store/web/user/service/UserService.java
+++ src/main/java/com/vinland/store/web/user/service/UserService.java
@@ -1,11 +1,35 @@
 package com.vinland.store.web.user.service;
 
+import com.vinland.store.utils.exception.EmailAlreadyExistsException;
 import com.vinland.store.web.user.dao.UserDAO;
+import com.vinland.store.web.user.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserDAO userDAO;
+
+    public Optional<User> loadUserByEmail(String email) {
+        return userDAO.findByEmail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return loadUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + username + " not found"));
+    }
+
+    public User create(User user) {
+        if (Boolean.TRUE.equals(userDAO.findByEmail(user.getEmail()))) {
+            throw new EmailAlreadyExistsException("User with email " + user.getEmail() + " already exists");
+        }
+        return userDAO.save(user);
+    }
 }
