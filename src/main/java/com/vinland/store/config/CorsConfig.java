bb588744be0e63fe69087a964552fd9747a93a25
EconLQ <zavaliybogdan@gmail.com>
2025-02-02 21:37:26 +0200

    updated cors adn web config to support JWT

--- src/main/java/com/vinland/store/config/CorsConfig.java
+++ src/main/java/com/vinland/store/config/CorsConfig.java
@@ -7,22 +7,23 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final var configuration = new CorsConfiguration();
-        final var urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 
-        configuration.setAllowedOrigins(Arrays.asList(PathConstants.FRONT_END_DEV, PathConstants.FRONT_END_PROD));
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
+        configuration.setAllowedOrigins(List.of(PathConstants.FRONT_END_DEV, PathConstants.FRONT_END_PROD));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+        configuration.setMaxAge(3600L);
 
-        return urlBasedCorsConfigurationSource;
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
 
