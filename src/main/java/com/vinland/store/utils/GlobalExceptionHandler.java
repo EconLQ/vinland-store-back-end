81b8602cc05353377316034b887d13848a5c12d1
EconLQ <zavaliybogdan@gmail.com>
2025-02-08 17:07:59 +0200

    handlers for new exceptions

--- src/main/java/com/vinland/store/utils/GlobalExceptionHandler.java
+++ src/main/java/com/vinland/store/utils/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.vinland.store.utils;
 
 import com.vinland.store.utils.exception.JwtTokenException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,5 +14,20 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(JwtTokenException.class)
     public ResponseEntity<MessageResponse> handleJwtTokenException(JwtTokenException ex) {
         return new ResponseEntity<>(new MessageResponse(ex.getMessage()), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<MessageResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ResponseEntity<>(new MessageResponse(ex.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<MessageResponse> handleMissingRequestAuthorizationHeaderException(MissingRequestHeaderException ex) {
+        return new ResponseEntity<>(new MessageResponse("Missing Authorization Header"), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<MessageResponse> handleAuthenticationException(AuthenticationException ex) {
+        return new ResponseEntity<>(new MessageResponse(ex.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 }
