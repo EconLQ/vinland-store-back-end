3e1fab3833e7542cb0ed2cb6984f64b735d3b053
EconLQ <zavaliybogdan@gmail.com>
2025-02-08 17:09:10 +0200

    updated generator with ADMIN authority

--- src/main/java/com/vinland/store/web/generator/GeneratorController.java
+++ src/main/java/com/vinland/store/web/generator/GeneratorController.java
@@ -1,24 +1,26 @@
 package com.vinland.store.web.generator;
 
+import com.vinland.store.utils.MessageResponse;
 import com.vinland.store.utils.PathConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin
 @RequestMapping(PathConstants.GENERATOR)
+@PreAuthorize("hasAuthority('ADMIN')")
 public class GeneratorController {
     private final GeneratorService generatorService;
 
     @PostMapping("/blogs")
-    //    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Void> generateBlogs(
+    public ResponseEntity<MessageResponse> generateBlogs(
             @RequestParam(required = false, defaultValue = "50") int amount
     ) {
         generatorService.generateBlogs(amount);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new MessageResponse("Generated " + amount + " blogs"));
     }
 }
 
