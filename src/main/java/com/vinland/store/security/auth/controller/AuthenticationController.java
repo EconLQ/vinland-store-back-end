9be300cf0d636836bf7d0c3b671ba9b00a45970d
EconLQ <zavliybogdan@gmail.com>
2025-02-11 20:43:06 +0200

    logout method and refactor sign-in/up endpoints

--- src/main/java/com/vinland/store/security/auth/controller/AuthenticationController.java
+++ src/main/java/com/vinland/store/security/auth/controller/AuthenticationController.java
@@ -6,6 +6,8 @@ import com.vinland.store.security.auth.model.RegistrationRequest;
 import com.vinland.store.security.auth.service.AuthenticationService;
 import com.vinland.store.utils.MessageResponse;
 import com.vinland.store.utils.PathConstants;
+import com.vinland.store.utils.exception.UserAlreadyExistException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -21,7 +23,7 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping(value = "/sign-in", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public ResponseEntity<LoginResponse> signInUser(@ModelAttribute LoginRequest loginRequest) {
+    public ResponseEntity<MessageResponse> signInUser(@ModelAttribute LoginRequest loginRequest) {
         final LoginResponse response = authenticationService.signIn(loginRequest.getEmail(), loginRequest.getPassword());
         final String cleanedAccessToken = response.getAccessToken().substring("Bearer ".length());
         ResponseCookie cookie = ResponseCookie.from("accessToken", cleanedAccessToken)
@@ -30,13 +32,31 @@ public class AuthenticationController {
                 .path("/")
                 .sameSite("None")
                 .build();
-        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(response);
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(new MessageResponse("User logged in successfully"));
     }
 
     @PostMapping(value = "/sign-up", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public MessageResponse signUpUser(@ModelAttribute RegistrationRequest request) {
-        authenticationService.signUp(request);
-        return new MessageResponse("User registered successfully");
+    public ResponseEntity<?> signUpUser(@Valid @ModelAttribute RegistrationRequest request) {
+        try {
+            authenticationService.signUp(request);
+            return ResponseEntity.ok(new MessageResponse("User registered successfully"));
+        } catch (UserAlreadyExistException e) {
+            return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
+        }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<MessageResponse> logout(@RequestHeader(value = "Cookie") String cookieHeader) {
+        if (cookieHeader == null || cookieHeader.isBlank() || !cookieHeader.contains("accessToken")) {
+            return ResponseEntity.ok().body(new MessageResponse("User is not logged in"));
+        }
+        ResponseCookie cookie = ResponseCookie.from("accessToken", null)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(0)
+                .build();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).body(new MessageResponse("Logged out successfully"));
     }
 
     @PostMapping(value = "/refresh")
